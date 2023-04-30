1e4f64f062740ca9e0da60fe0560c6fb7d65760c
Mahardika Pratama <117805307+MahardikaPratama@users.noreply.github.com>
2023-04-30 10:27:21 +0700

    Update Hitung freq

--- ReadTest.cpp
+++ ReadTest.cpp
@@ -29,20 +29,19 @@ void readImage(unsigned char header[], unsigned char* &image) // perubahan pada 
 {
 	FILE *read, *write1;
   	int i, j;
-	read = fopen("first.ppm", "rb"); /* b - binary mode */
+	read = fopen("sawah.ppm", "rb"); /* b - binary mode */
 	
     readPPMHeader(read, header);
     if (header[0]!='P' || header[1]!='6'){
         printf("Wrong hahah");
         return;
-    }
+    }	
     // get dimenstion
     int width, height, clrs, pos = 3;
     width = getDimension(header, pos);
     pos++;
     
     height = getDimension(header, pos);
-    printf("Width : %d, Height : %d", width, height);
     image = new unsigned char [width * height * 3];
     readPPMImage(read, image, width*height*3);
     // Validate the pixel intensities (rgb) of the first pixel. (255 29 29)
@@ -50,33 +49,51 @@ void readImage(unsigned char header[], unsigned char* &image) // perubahan pada 
     fclose(read);
 }
 
-void hitungFreq(unsigned char image[][3], Huffman *data, int imageSize){
-	printf("Frekuensi\tR\tG\tB \n");
-	
-	bool visited[9] = {false};
-	int size = imageSize;
+void countPixelFrequency(unsigned char* image, int width, int height, HuffmanNode* frequencyTable){
+	*frequencyTable = new tHuffmanNode[256 * 256 * 256]();
+	for (int i = 0; i < 256 * 256 * 256; i++) {
+        (*frequencyTable)[i].freq = 0;
+    }
+
+    int size = width * height * 3;
+    bool* visited = new bool[size] {false};
+    printf("\nsize : %d\n", size);
 	int count;
 	int j,k;
-	for (int i = 0 ; i < size; i++){
+	int index = 0;
+	for (int i = 0 ; i < size; i+=3){
+		bool found = false;
 		if(visited[i] == true){
 			continue;
 		}
 		count = 1;
-		for (j=i+1; j < size; j++){
-			if (image[i][0] == image[j][0] && image[i][1] == image[j][1] && image[i][2] == image[j][2]){
-				visited[j] = true;
-				count++;
-			}
-		}
 		
-		data->info[0] = image[i][0];
-		data->info[1] = image[i][1];
-		data->info[2] = image[i][2];
-		data->freq = count;
-	printf("%d\t\t%d\t%d\t%d\n", data->freq, data->info[0], data->info[1], data->info[2]);	
+		for (j = i + 3; j < size; j += 3) {
+            if (image[i] == image[j] && image[i + 1] == image[j + 1] && image[i + 2] == image[j + 2]) {
+                visited[j] = true;
+                count++;
+            }
+        }
+		
+		(*frequencyTable)[index].freq = count;
+        (*frequencyTable)[index].info[0] = image[i];
+        (*frequencyTable)[index].info[1] = image[i+1];
+        (*frequencyTable)[index].info[2] = image[i+2];
+		index++;
+		
 	}
-	
+	delete[] visited;
 }
 
-
+// function to print pixel frequency table
+void printPixelFrequency(HuffmanNode frequencyTable){
+    printf("\nPixel Frequency Table:\n");
+	for(int i=0; i<256*256*256; i++){
+        if(frequencyTable[i].freq > 0){
+            printf("(%d, %d, %d) : %d\n", 
+                   frequencyTable[i].info[0], frequencyTable[i].info[1], frequencyTable[i].info[2],
+                   frequencyTable[i].freq);
+        }
+    }
+}
 
